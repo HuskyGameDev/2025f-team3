5f0c5cef3c4bb8676102be4a61198e390b497c35
kmsatck <156161267+kmsatck@users.noreply.github.com>
2025-10-05 11:02:16 -0400

    Added a scene for player, tower, and enemy
    Added a scene for player, tower, and enemy with short codes.

---
+++ crosscut/enemy1_stats.gd
@@ -0,0 +1,8 @@
+extends CharacterBody3D
+
+# just put variables that I can think of.
+# let me know if there's more that need to be added or deleted.
+@export var hp = 10
+@export var speed = 10
+@export var atk = 10
+@export var atk_speed = 5
