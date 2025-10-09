7cdd48d59c9268c328d06fc8ef34ed204f8dcd01
Collin McFarland <cmcfarla@mtu.edu>
2025-10-08 21:48:20 -0400

    Settings
    Settings menu creates a config file that saves some settings. It saves directly to the assets directory right now because I wasn't sure what folder it could fit in.
    
    There are also some minor changes to the 2D HUD to make it easier to work with, like proper component names.

--- crosscut/assets/UI/settings_menu.gd
+++ crosscut/assets/UI/settings_menu.gd
@@ -1,7 +1,34 @@
 extends PanelContainer
 
+var main_volume
+var music_volume
+var sfx_volume
+
+var settings
+
 func _ready() -> void:
-	pass # Settings would be loaded from a prefs file in this function
+	settings = ConfigFile.new()
+	var main_volume = $BoxContainer/MainVolume/HSlider.value
+	var music_volume = $BoxContainer/MusicVolume/HSlider.value
+	var sfx_volume = $BoxContainer/SFXVolume/HSlider.value
+	
+	if settings.load("res://assets/settings.cfg") != OK:
+		settings.set_value("settings", "main_volume", main_volume)
+		settings.set_value("settings", "music_volume", music_volume)
+		settings.set_value("settings", "sfx_volume", sfx_volume)
+		
+		settings.save("res://assets/settings.cfg")
+		
+		return
+	
+	main_volume = settings.get_value("settings", "main_volume")
+	music_volume = settings.get_value("settings", "music_volume")
+	sfx_volume = settings.get_value("settings", "sfx_volume")
+	$BoxContainer/MainVolume/HSlider.value = main_volume
+	$BoxContainer/MusicVolume/HSlider.value = music_volume
+	$BoxContainer/SFXVolume/HSlider.value = sfx_volume
+	
+	settings.save("res://assets/settings.cfg")
 
 func _on_back_pressed() -> void:
 	visible = false
@@ -11,12 +38,33 @@ func _on_back_pressed() -> void:
 
 # Main volume slider
 func _on_h_slider_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	main_volume = value
+	$BoxContainer/MainVolume/HSlider.value = main_volume
+	settings.set_value("settings", "main_volume", main_volume)
+	settings.save("res://assets/settings.cfg")
 
 # Music volume slider
 func _on_h_slider_value_changed_music(value: float) -> void:
-	pass # Replace with function body.
+	music_volume = value
+	$BoxContainer/MusicVolume/HSlider.value = music_volume
+	settings.set_value("settings", "music_volume", music_volume)
+	settings.save("res://assets/settings.cfg")
 
 # SFX volume slider
 func _on_h_slider_value_changed_sfx(value: float) -> void:
-	pass # Replace with function body.
+	sfx_volume = value
+	$BoxContainer/SFXVolume/HSlider.value = sfx_volume
+	settings.set_value("settings", "sfx_volume", sfx_volume)
+	settings.save("res://assets/settings.cfg")
+
+func _on_reset_pressed() -> void:
+	main_volume = 100
+	music_volume = 100
+	sfx_volume = 100
+	$BoxContainer/MainVolume/HSlider.value = main_volume
+	$BoxContainer/MusicVolume/HSlider.value = music_volume
+	$BoxContainer/SFXVolume/HSlider.value = sfx_volume
+	settings.set_value("settings", "main_volume", main_volume)
+	settings.set_value("settings", "music_volume", music_volume)
+	settings.set_value("settings", "sfx_volume", sfx_volume)
+	settings.save("res://assets/settings.cfg")
