f5c91714d4bd0c3747794846a1dfd58ab56f5fb0
kmsatck <156161267+kmsatck@users.noreply.github.com>
2025-10-30 15:31:31 -0400

    Fixed few error with the last push
    There was a few error with my last push, so I fixed the one I was able and commented out the part I don't know why the error is happening. If anyone knows why and have an idea how to fix it, let me know.

--- crosscut/assets/scripts/enemy1_stats.gd
+++ crosscut/assets/scripts/enemy1_stats.gd
@@ -23,26 +23,28 @@ func _input(event):
 		print("KILLING ENEMY")
 		destroy()
 
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-		if body.is_in_group("player"):
+# enemy attack, currently only deal damage when collide with the player
+func _on_damage_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
 		body.take_damage(atk)
 
+"""
 # from down here, it is enemy movement
 @onready var player = %Player
 
 func _physics_process(delta):
-		var direction = (player.global_transform.origin - global_transform.origin)
-		direction.y = 0
-		var distance = direction.length()
+	var direction = (player.global_transform.origin - global_transform.origin)
+	direction.y = 0
+	var distance = direction.length()
 
-		if distance > 0:
-			var move_dir = direction.normalized()
-			velocity = move_dir * speed
-			move_and_slide()
+	if distance > 0:
+		var move_dir = direction.normalized()
+		velocity = move_dir * speed
+		move_and_slide()
 
-			var target_rotation = Vector3(0, atan2(move_dir.x, move_dir.z), 0)
-			rotation.y = lerp_angle(rotation.y, target_rotation.y, rotation_speed * delta)
-		else:
-			velocity = Vector3.ZERO
-			move_and_slide()
+		var target_rotation = Vector3(0, atan2(move_dir.x, move_dir.z), 0)
+		rotation.y = lerp_angle(rotation.y, target_rotation.y, rotation_speed * delta)
+	else:
+		velocity = Vector3.ZERO
+		move_and_slide()
+"""
