a1295ebda55b233ab5f3114de84efd18d7f313e6
rophillimtu <rophilli@mtu.edu>
2026-01-27 17:11:33 -0500

    Preloaded hit sounds

--- crosscut/assets/scripts/AudioManager.gd
+++ crosscut/assets/scripts/AudioManager.gd
@@ -12,10 +12,20 @@ func _ready() -> void:
 	add_child(music_player)
 	
 	sfx_dict["ui_click"] = preload("res://assets/audio/sfx/ui button click/ui button click.wav")
+	
 	sfx_dict["crossbow1"] = preload("res://assets/audio/sfx/Crossbow/Crossbow Firing - 1.wav")
 	sfx_dict["crossbow2"] = preload("res://assets/audio/sfx/Crossbow/Crossbow Firing - 2.wav")
 	sfx_dict["crossbow3"] = preload("res://assets/audio/sfx/Crossbow/Crossbow Firing - 3.wav")
-		
+	
+	sfx_dict["enemy_hit1"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit1.wav")
+	sfx_dict["enemy_hit2"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit2.wav")
+	sfx_dict["enemy_hit3"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit3.wav")
+	sfx_dict["enemy_hit4"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit4.wav")
+	sfx_dict["enemy_hit5"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit5.wav")
+	sfx_dict["enemy_hit6"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit6.wav")
+	sfx_dict["enemy_hit7"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit7.wav")
+	sfx_dict["enemy_hit8"] = preload("res://assets/audio/sfx/Goblin Hit/Goblin Hit8.wav")
+	
 	score_dict["menu"] = preload("res://assets/audio/music/Crosscut Main Theme Demo.mp3")
 	score_dict["placement"] = preload("res://assets/audio/music/Crosscut Placement Theme.mp3")
 	
