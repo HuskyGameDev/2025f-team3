9280559b8f8bbdc99c66888d4290be54cec462a2
Ezimmz <eazclone@gmail.com>
2026-02-18 16:17:58 -0500

    Misc. Balance changes, Enemy 2 fix, and ballista tower selectable

--- crosscut/assets/scripts/levelData.gd
+++ crosscut/assets/scripts/levelData.gd
@@ -12,14 +12,15 @@ static var DEFAULT_LEVELS: Array = [
 		],
 		# Wave 2
 		[
-			spawn(8, "test1", ["west","east"], 0.4)
+			spawn(12, "test1", ["west","east"], 0.4)
 		]
 	],
 	# Level 2
 	[
 		# Wave 1
 		[
-			spawn(20, "test1", ["south", "north", "east", "west"], 0.4)
+			spawn(1, "test2", ["west"], 1.0),
+			spawn(20, "test1", ["south", "north", "east", "west"], 0.2)
 		],
 		# Wave 2
 		[
@@ -30,38 +31,90 @@ static var DEFAULT_LEVELS: Array = [
 	[
 		# Wave 1
 		[
-			spawn(3, "test1", ["east"], 0.1),
-			spawn(3, "test1", ["north"], 0.1),
-			spawn(3, "test1", ["west"], 0.1)
+			spawn(5, "test1", ["east"], 0.1),
+			spawn(5, "test1", ["north"], 0.1),
+			spawn(5, "test1", ["west"], 0.1)
 		],
 		# Wave 2
 		[
-			spawn(6, "test1", ["south"], 0.3)
+			spawn(8, "test1", ["south"], 0.3)
 		],
 		# Wave 3
 		[
-			spawn(5, "test1", ["north"], 1)
+			spawn(1, "test3", ["north"], 1.0)
 		]
 	],
 	# Level 4
 	[
 		# Wave 1
 		[
-			spawn(3, "test1", ["north"], 0.1),
+			spawn(5, "test1", ["north"], 0.1),
 			spawn(1, "test3", ["north"], 1.0),
-			spawn(3, "test1", ["east"], 0.1),
+			spawn(5, "test1", ["east"], 0.1),
 			spawn(1, "test3", ["east"], 1.0),
-			spawn(3, "test1", ["south"], 0.1),
+			spawn(5, "test1", ["south"], 0.1),
 			spawn(1, "test3", ["south"], 1.0),
-			spawn(3, "test1", ["west"], 0.1),
+			spawn(5, "test1", ["west"], 0.1),
 			spawn(1, "test3", ["west"], 1.0),
 		],
 		# Wave 2
 		[
-			spawn(16, "test1", ["west"], 0.3),
-			#spawn(1, "test2", ["west"], 1.0) TODO: uncomment when enemy 2 targets player
+			spawn(9, "test1", ["west"], 0.3),
+			spawn(3, "test2", ["east"], 1.0)
 		]
-	]
+	],
+	# Level 5 - boss wave
+	[
+		# Wave 1
+		[
+			spawn(12, "test1", ["south", "north", "east", "west"], 1.3),
+			spawn(24, "test1", ["south", "north", "east", "west"], 0.5),
+			spawn(12, "test2", ["south", "north", "east", "west"], 0.7),
+			spawn(8, "test3", ["south", "north", "east", "west"], 0.5)
+		]
+	],
+	# Level 6
+	[
+		# Wave 1
+		[
+			spawn(2, "test3", ["east"], 5.0),
+			spawn(2, "test3", ["south"], 5.0),
+			spawn(2, "test3", ["west"], 5.0),
+			spawn(2, "test3", ["north"], 5.0)
+		],
+		# Wave 2
+		[
+			spawn(1, "test2", ["east"], 5.0)
+		],
+		# Wave 3
+		[
+			spawn(1, "test2", ["east"], 5.0)
+		],
+		# Wave 4
+		[
+			#evil wave
+			spawn(31, "test1", ["east"], 0.01)
+		]
+	],
+	# Level 7
+	[
+		# Wave 1
+		[
+			spawn(3, "test2", ["north"], 0.4),
+			spawn(1, "test3", ["north"], 1.0),
+			spawn(3, "test2", ["east"], 0.4),
+			spawn(1, "test3", ["east"], 1.0),
+			spawn(3, "test2", ["south"], 0.4),
+			spawn(1, "test3", ["south"], 1.0),
+			spawn(3, "test2", ["west"], 0.4),
+			spawn(1, "test3", ["west"], 1.0),
+		],
+		# Wave 2
+		[
+			spawn(5, "test1", ["west"], 0.01),
+			spawn(5, "test1", ["east"], 0.01),
+		]
+	],
 ]
 
 static func getDefaultLevel(levelIndex: int) -> Array:
