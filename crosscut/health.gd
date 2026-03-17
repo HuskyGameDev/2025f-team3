fc04c628a0d3bffc03380128c0d5569f0475283c
Ezimmz <eazclone@gmail.com>
2026-03-17 14:12:46 -0400

    Added toggle for particles in health.tscn

--- crosscut/health.gd
+++ crosscut/health.gd
@@ -5,6 +5,7 @@ signal killed_sig
 
 @export var debug: bool = false
 @export var max_health: int
+@export var has_particles: bool
 @export var hitFX: GPUParticles3D #assigned as child in editor
 
 @onready var health: float = max_health
@@ -16,7 +17,10 @@ func take_damage(damage: int) -> void:
 	if debug: print("Current health is ", health)
 	health -= damage
 	damaged_sig.emit(damage, health)
-	#hitFX.restart() #display hit particle FX
+	
+	if has_particles:
+		hitFX.restart() #display hit particle FX
+	
 	if debug: print("Health after damage: ", health, " (max: ", max_health, ")")
 	if health <= 0:
 		if debug: print("HEALTH ZERO - EMITTING KILLED SIGNAL for: ", get_parent().name)
